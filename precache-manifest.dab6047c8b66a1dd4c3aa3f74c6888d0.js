09943dcb7772148ed44d85996a6483046333c133
jason-hu7 <dcjasonhu@gmail.com>
2020-05-16 12:15:37 -0500

    Updates

---
+++ precache-manifest.dab6047c8b66a1dd4c3aa3f74c6888d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7795522c9aef7068a10bf9aba5665e",
+    "url": "/husweb/index.html"
+  },
+  {
+    "revision": "1035680a568ff8aa04aa",
+    "url": "/husweb/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ede30bc0f1cfe9c13a41",
+    "url": "/husweb/static/js/2.d0eb04a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/husweb/static/js/2.d0eb04a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1035680a568ff8aa04aa",
+    "url": "/husweb/static/js/main.37d3b651.chunk.js"
+  },
+  {
+    "revision": "299026cdd99fe088ee90",
+    "url": "/husweb/static/js/runtime-main.0a90d7dd.js"
+  }
+]);
