87d0449f38791a5958744940fcf0242cc1473aac
jason-hu7 <dcjasonhu@gmail.com>
2020-09-17 19:20:46 -0500

    putting the alts on imgs

--- src/components/hobbies.js
+++ src/components/hobbies.js
@@ -28,25 +28,25 @@ export default class Hobbies extends Component {
           .
         </p>
         <p style={{ margin: "0px 400px 50px 400px" }}>
-          Also, Nando's 🐓 is maybe probably possibly the best fast food in the
+          Also, Nando's <span role="img" aria-label="chicken">🐓</span> is maybe probably possibly the best fast food in the
           world.
         </p>
         <div className="row">
           <div className="column">
-            <img src={process.env.PUBLIC_URL + "/assets/cooking.jpg"} />
-            <img src={process.env.PUBLIC_URL + "/assets/basketball.jpg"} />
+            <img src={process.env.PUBLIC_URL + "/assets/cooking.jpg"} alt="cooking"/>
+            <img src={process.env.PUBLIC_URL + "/assets/basketball.jpg"} alt="basketball"/>
           </div>
           <div className="column">
-            <img src={process.env.PUBLIC_URL + "/assets/snowboarding.jpg"} />
-            <img src={process.env.PUBLIC_URL + "/assets/nandos.jpg"} />
+            <img src={process.env.PUBLIC_URL + "/assets/snowboarding.jpg"} alt="snowboarding"/>
+            <img src={process.env.PUBLIC_URL + "/assets/nandos.jpg"} alt="nandos"/>
           </div>
           <div className="column">
-            <img src={process.env.PUBLIC_URL + "/assets/climbing.jpg"} />
-            <img src={process.env.PUBLIC_URL + "/assets/cycling.jpg"} />
+            <img src={process.env.PUBLIC_URL + "/assets/climbing.jpg"} alt="outdoor climbing"/>
+            <img src={process.env.PUBLIC_URL + "/assets/cycling.jpg"} alt="biking with ben"/>
           </div>
           <div className="column">
-            <img src={process.env.PUBLIC_URL + "/assets/fam.jpg"} />
-            <img src={process.env.PUBLIC_URL + "/assets/motorcycle.jpg"} />
+            <img src={process.env.PUBLIC_URL + "/assets/fam.jpg"} alt="my fam"/>
+            <img src={process.env.PUBLIC_URL + "/assets/motorcycle.jpg"} alt="motorcycle"/>
           </div>
         </div>
       </div>
