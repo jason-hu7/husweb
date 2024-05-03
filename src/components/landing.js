c2a4d5f365e40cd82f34e10f111d8f83d3cf0fd6
jason-hu7 <dcjasonhu@gmail.com>
2024-05-02 17:22:12 -0700

    update landing

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,52 +1,52 @@
 import React, { Component } from "react";
 
 export default class Landing extends Component {
-  render() {
-    return (
-      <header className="header">
-        <div className="content">
-          <div>
-            <h1>Jason Hu</h1>
-            <p style={{ marginBottom: "4vh" }}>Data Scientist</p>
-            <a
-              href="https://github.com/jason-hu7"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={process.env.PUBLIC_URL + "/assets/GitHub-Mark.png"}
-                className="button"
-                alt="github logo"
-              />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/jason-hu-94378758/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={process.env.PUBLIC_URL + "/assets/linkedin-logo.png"}
-                className="button-li"
-                alt="linkedin logo"
-              />
-            </a>
-            <a
-              href="mailto:dcjasonhu@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={process.env.PUBLIC_URL + "/assets/email-icon.png"}
-                className="button"
-                alt="email icon"
-              />
-            </a>
-          </div>
-          <a className="pulse" href="#about">
-            ˇ
-          </a>
-        </div>
-      </header>
-    );
-  }
+    render() {
+        return (
+            <header className="header">
+                <div className="content">
+                    <div>
+                        <h1>Jason Hu</h1>
+                        <p style={{ marginBottom: "4vh" }}>Software Engineer</p>
+                        <a
+                            href="https://github.com/jason-hu7"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={process.env.PUBLIC_URL + "/assets/GitHub-Mark.png"}
+                                className="button"
+                                alt="github logo"
+                            />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/jason-hu-94378758/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={process.env.PUBLIC_URL + "/assets/linkedin-logo.png"}
+                                className="button-li"
+                                alt="linkedin logo"
+                            />
+                        </a>
+                        <a
+                            href="mailto:dcjasonhu@gmail.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img
+                                src={process.env.PUBLIC_URL + "/assets/email-icon.png"}
+                                className="button"
+                                alt="email icon"
+                            />
+                        </a>
+                    </div>
+                    <a className="pulse" href="#about">
+                        ˇ
+                    </a>
+                </div>
+            </header>
+        );
+    }
 }
